b1a2437ae857f6e220408a4d9ce4e63e45b9ecaa
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2013-04-05 09:03:15 -0300

    work in progress

--- lineup.c
+++ lineup.c
@@ -1,22 +1,72 @@
 //SPOJ problem : LINEUP
-int main(){
-  //int p1[11];
-  //int p2[11];
-  //int p3[11];
-  //int p4[11];
-  //int p5[11];
-  //int p6[11];
-  //int p7[11];
-  //int p8[11];
-  //int p9[11];
-  //int p10[11];
-  //int p11[11];
-  int n_tests ;
-  scanf(n_tests);
-  int i;
-  for(i = 0; i < n_tests; i++){
-    
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int* string_to_array(char *si){
+  char *aux = malloc(sizeof(char)*strlen(si));
+  int nws = n_whitespaces(si);
+  strcpy(aux,si);
+  char *rest;
+  char *token;
+  int *a = malloc(sizeof(int)*nws);
+  int i = 0;
+  while(token = strtok_r(aux," ",&rest)){
+    aux = rest;
+    a[i] = atoi(token);
+    i++;
   }
+  free(aux);
+  return a;
+}
+
+int n_whitespaces(char* s){
+  char *aux = malloc(sizeof(char)*strlen(s));
+  strcpy(aux,s);
+  char *token;
+  char *rest;
+  int count = 0;
+  token = strtok_r(aux," ",&rest);
+  aux = rest;
+  while(token = strtok_r(aux," ",&rest)){
+    aux = rest;
+    count++;
+  }
+  if(count < 0){
+    count = 0;
+  }
+  free(aux);
+  return count;
+}
+
+void print_array(void *a){
+  int i;
+  for(i = 0; i < sizeof(a)/sizeof(a[0]); i++){
+    if(sizeof(a[0]) == sizeof(int)){
+      printf("%i",(*(int*)a)[0]);
+    }
+    if(sizeof(a[0]) == sizeof(char)){
+      printf("%s",(*(int*)a)[0]);
+    }
+  }
+  printf("\n",NULL);
+}
+
+int main(){
+
+  //testing n_whitespaces
+  //char *s = malloc(sizeof(char)*256);
+  //scanf("%[^\n]%*c",s);
+  //printf("n_whitespaces = %i",n_whitespaces(s));
+  //free(s);
+
+  //testing string to array
+  char *s = malloc(sizeof(char)*256);
+  scanf("%[^\n]%*c",s);
+  int *i = string_to_array(s);
+  print_array(i);
+  free(s);
+  free(i);
   return 0;
 }
 
